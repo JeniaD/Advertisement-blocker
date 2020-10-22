332cb3368212c36bdd27d23a2dad46c3fc7c6b94
JeniaD <53406085+JeniaD@users.noreply.github.com>
2020-10-22 15:09:32 +0300

    Update background.js

--- background.js
+++ background.js
@@ -1,5 +1,5 @@
 chrome.webRequest.onBeforeRequest.addListener(
   function(details) { console.log("Site blocked.");return {cancel: true}; },
-  { urls: ["*://*.doubleclick.net/*", "*://*.broadstreetads.com/*", "*://*.adbutler.com/*", "*://*.zedo.com/*", "*://*.media.net/*", "*://*.yandex.ru/*", "*://*.rutrk.com/*", "*://.vdo.ai/*", "*://*.traffic-media.co.uk/*","*://*.acdnpro.com/*"/*, "*//*.trafmag.com/*"*/]},
+  { urls: ["*://*.doubleclick.net/*", "*://*.broadstreetads.com/*", "*://*.adbutler.com/*", "*://*.zedo.com/*", "*://*.media.net/*", "*://*.yandex.ru/*", "*://*.rutrk.com/*", "*://.vdo.ai/*", "*://*.traffic-media.co.uk/*", "*://*.acdnpro.com/*", "*://*.wisokykulas.bid/*", "*://*.ankunding.biz/*", "*://*.all-cod.com/*", "*://*.mrelko.com/*", "*://*.zgxrht.com/*", "*://*.recreativ.ru/*", "*://*.steepto.com/*", "*://rstbtmd.com/*", "*://s.zone.nogravitycdn.com/*"]},
   ["blocking"]
 );
