ff93963f6b9d3b826440cce4004a4fec0e527e68
JeniaD <53406085+JeniaD@users.noreply.github.com>
2020-10-22 15:07:44 +0300

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,7 +1,7 @@
 function Cmd(){
 	command = document.getElementById("text-input").value;
 	if(command == "help" || command == "info" || command == "?")
-		document.getElementById("new-translation").innerHTML = "<b>Help - Show this info.<br>Destroy - clear \"iframes\".<br>Clear - clear this.</b>";
+		document.getElementById("new-translation").innerHTML = "<b>Help - Show this info.<br>Clear - clear this.</b>";
 	else if(command == "clear")
 		document.getElementById("new-translation").innerHTML = "";
 	//else
@@ -13,27 +13,3 @@ setTimeout(() => {
 	var object = document.getElementById("text-input");
 	object.addEventListener("change", Cmd());
 }, 3000);
-
-
-/*document.addEventListener('DOMContentLoaded', function () {
-  document.querySelector('button').addEventListener('click', onclick, false)
-  
-  function onclick () {
-    chrome.tabs.query({currentWindow: true, active: true}, function (tabs) {
-		//alert("Worked");
-      chrome.tabs.sendMessage(tabs[0].id, 'Are you sure?', setCount)
-		
-    })
-  }
-  
-  /*function setCount (res) {
-    const div = document.createElement('div')
-    div.textContent = `${res.count} bears`
-    document.body.appendChild(div)
-  }*/
-/*}, false)(*/
-
-/*chrome.browserAction.onClicked.addListener(function (tab) {
-  chrome.tabs.create({url: 'popup.html'})
-  alert("hi");
-})*/
